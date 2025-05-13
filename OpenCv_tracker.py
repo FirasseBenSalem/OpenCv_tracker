4d9d559095b5bdf71d60c45e97569fd32beb7c07
FirasseBenSalem <17160@edu.campusdetandem.be>
2025-05-13 15:59:13 +0200

    Update OpenCv_tracker.py
    13/05
    circle detection

--- OpenCv_tracker.py
+++ OpenCv_tracker.py
@@ -1,36 +1,43 @@
 import cv2 as cv
 import numpy as np
-import time
-import datetime
 
-cap = cv.VideoCapture(1)
-
+videoCapture = cv.VideoCapture(0)
+prevCircle = None
+dist = lambda x1,y1,x2,y2: (x1-x2)**2+(y1-y2)**2
 while True:
-    current_time = str(datetime.datetime.now())
+    ret,frame = videoCapture.read()
+    if not ret: break
+    
+    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    blurFrame = cv.GaussianBlur(grayFrame, (17,17), 0)
+    
+    circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT,1.2,100,param1=100,param2=30,minRadius=50,maxRadius=75)
+    
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        chosen = None
+        for i in circles[0,:]:
+            if chosen is None :
+                chosen = i
+            if prevCircle is not None :
+                if dist(chosen[0],chosen[1],prevCircle[0],prevCircle[1]) <= dist(i[0],i[1],prevCircle[0],prevCircle[1]):
+                    chosen = i
+        cv.circle(frame, (chosen[0],chosen[1]),1,(0,100,100),3)
+        cv.circle(frame,(chosen[0],chosen[1]),chosen[2],(255,0,255),3)
+        prevCircle = chosen
+    
+    cv.imshow("circles",frame)
+            
     
     
-         
-    HOUR        = datetime.datetime.now().hour   
-    MINUTE      = datetime.datetime.now().minute
-    
-    ret,frame = cap.read()
-    
-    if not ret:
-        break
-    width = int(cap.get(3))
-    height = int(cap.get(4))
     
     
     
-    font = cv.FONT_HERSHEY_COMPLEX
-    cv.putText(frame,f"{HOUR}:{MINUTE}", (0,30),font,1,(255,0,0),1,cv.LINE_AA)
+    if cv.waitKey(1) == (27): break
     
-    
-    
-    
-    cv.imshow('frame',frame)
-    
-    if cv.waitKey(1) == 27:
-        break
-cap.release()
+videoCapture.release()
 cv.destroyAllWindows()
+    
+    
+        
+        
